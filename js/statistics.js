73ecd662b216a4b5bd318390a0d06a093b7bc638
王振 <kill370354@qq.com>
2019-03-18 13:49:56 +0800

    Add files via upload

---
+++ js/statistics.js
@@ -0,0 +1,32 @@
+(function () {
+    function getCookie(cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i].trim();
+            if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+        }
+        return 0;
+    }
+    if (getCookie("time")) return;
+    if (navigator.userAgent.indexOf("kill370354mixed") == -1) {
+        var data = "{" + (new Date()).valueOf() + "}";//localStorage存储的数据必须使用双引号，采用反斜杠转义，否则JSON.parse()解析时会报错！
+        var storage = window.localStorage;
+        var interval = 3700000;
+
+        function setCookie(cname, cvalue, exdays) {
+            var d = new Date();
+            d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+            var expires = "expires=" + d.toGMTString();
+            document.cookie = cname + "=" + cvalue + "; " + expires;
+        }
+        setCookie("time", 1, 0.5);//存储数据
+        //fnCreateRequest("js/GetViwerInfo.php?url=" + window.location.href);
+
+        fnCreateRequest("//a.yangy97.top/js/GetViwerInfo.php", function (result) {
+//console.log(result);
+            console.log("欢迎访问本站！");
+        });
+    }
+})();
+
---
+++ js/xhr.js
@@ -0,0 +1,51 @@
+var oXMLHttpRequest = false;
+
+function fnCreateRequest(url,fnXHRCompleted) {   //初始化对象并发出XMLHttpRequest请求
+    oXMLHttpRequest = false;
+    if (window.XMLHttpRequest) {   //Mozilla等其他浏览器
+        oXMLHttpRequest = new XMLHttpRequest();
+        //修复某些版本的火狐浏览器的bug
+        if (oXMLHttpRequest.overrideMimeType) {
+            oXMLHttpRequest.overrideMimeType("text/xml");
+        }
+    } else if (window.ActiveXObject) {     //IE浏览器
+        try {
+            oXMLHttpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+            try {
+                oXMLHttpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (e) {
+                console.log("无法创建xhr对象！");
+            }
+        }
+    }
+    if (!oXMLHttpRequest) {
+        alert("不能创建XMLHTTP实例，请截图联系kill370354@qq.com");
+        return false;
+    }
+
+    if(!fnXHRCompleted) {
+         fnXHRCompleted=function () {
+            console.log("ajax完成！");
+        }
+    }
+
+
+    function fnXHRRsponse() {   	 //处理服务器返回的信息
+        //   var oUserTip = document.getElementById("usertip");
+        if (oXMLHttpRequest.readyState == 4) {
+            if (oXMLHttpRequest.status == 200) {
+                fnXHRCompleted();
+            }
+        }
+    }
+    oXMLHttpRequest.onreadystatechange = fnXHRRsponse;
+    sUrl = url.match(/.*?\.php/)[0];
+    sRequest = url.substring(sUrl.length + 1)+"&timestamp=" + new Date().getTime();
+    // console.log(url);   console.log(sRequest);
+    // console.log(sUrl);
+    //指定响应方法函数
+    oXMLHttpRequest.open("POST", sUrl, true);
+    oXMLHttpRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    oXMLHttpRequest.send(sRequest);
+};
